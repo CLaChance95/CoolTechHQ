7e9fb883631a36f7135c25f1af3f9e40468b49e8
CLaChance95 <chris.leechance@gmail.com>
2025-08-05 12:33:09 -0500

    Create capacitor.config.ts

---
+++ cool-tech-hq-4c77b7b2-main/capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.cooltechhq.app',
+  appName: 'CoolTechHQ',
+  webDir: 'dist',
+  bundledWebRuntime: false
+};
+
+export default config;
